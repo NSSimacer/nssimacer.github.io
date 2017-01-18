f3adfab8845acf79f2f97cda36108116b815b6cf
NSSimacer <wuxiaoqiang1020@gmail.com>
2017-01-18 16:56:29 +0800

    Site updated: 2017-01-18 16:56:27

---
+++ js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a4be89823ce63d8356581b659877c513";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
